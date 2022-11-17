d4c52ac998c398e8be4c9fbc307c4b69109ae489
Rha02 <romuzus08@gmail.com>
2022-11-16 19:10:34 -0800

    Connecting to Database

---
+++ config.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = 'mysql:///'+os.environ.get("DB_CONNECTION")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # DB_CONNECTION = os.environ.get("DB_CONNECTION")
+    # DB_USER = os.environ.get("DB_USER")
+    # DB_PASSWORD = os.environ.get("DB_PASSWORD")
+    # DB_PORT = os.environ.get("DB_PORT")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
