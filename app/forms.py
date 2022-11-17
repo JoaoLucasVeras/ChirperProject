1f396662132e2a60714a7cfe4848849979933054
tankhanhdao <tankhanhdao@gmail.com>
2022-11-16 17:46:00 -0800

    added login template

--- app/forms.py
+++ app/forms.py
@@ -2,9 +2,18 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
-class LoginForm(FlaskForm):
+class LogIn_Form(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign In')
 
+class SignUp_Form(FlaskForm):
+    first_name = StringField('first_name')
+    last_name = StringField('last_name')
+    username = StringField('username')
+    email = StringField('email')
+    password = PasswordField('password')
+    cofirm_password = PasswordField('Confirm Password')
+
+    submit = SubmitField('sign up')
